3a396f119b71914f453d265676198c63dbd7e1ec
impreet01 <impreet01@gmail.com>
2025-10-31 09:23:12 -0700

    Remove custom logger bootstrap

--- ci/setupLogger.js
+++
@@ -1,8 +0,0 @@
-import logger, { installLogger } from '../logger.js';
-
-const bindings = {
-  scope: process.env.GITHUB_WORKFLOW || 'ci',
-  job: process.env.GITHUB_JOB,
-};
-
-installLogger({ logger, bindings });
